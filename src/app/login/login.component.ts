4b9c3ee7898b04024d1d0042ad393f029fd1e2da
Broggoli <broggoli.nb@gmail.com>
2018-07-23 23:42:05 +0200

    ts corrections

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {  Validators,
           FormGroup,
-          FormControl } from '@angular/forms'
-import { AuthService } from '../_services'
-import { Router } from "@angular/router"
+          FormControl } from '@angular/forms';
+import { AuthService } from '../_services';
+import { Router } from '@angular/router';
 
 interface ZiviDBObj {
-  ziviDataHeader: string,
-  encryptedZiviData: string,
-  expirationDate: number
+  ziviDataHeader: string;
+  encryptedZiviData: string;
+  expirationDate: number;
 }
-interface loginData{
-  success: boolean,
-  data: ZiviDBObj,
-  message: string
+/*
+interface LoginData {
+  success: boolean;
+  data: ZiviDBObj;
+  message: string;
 }
+*/
 
 @Component({
   selector: 'login',
@@ -23,80 +25,79 @@ interface loginData{
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup
-  email: FormControl
-  password: FormControl
-  loginError: string = ""
+  loginForm: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  loginError = '';
 
   constructor(private Auth: AuthService,
               private router: Router) { }
 
   ngOnInit() {
-    //Auto log in from index if data is already safed in localStorage
-    if(this.Auth.isLoggedIn){
-      this.router.navigate(["rapportblatt"])
-    }else{
-      document.querySelector("#logOutButton").classList.add("loggedOut")
-      this.createFormControls()
-      this.createForm()
+    // Auto log in from index if data is already safed in localStorage
+    if (this.Auth.isLoggedIn) {
+      this.router.navigate(['rapportblatt']);
+    } else {
+      document.querySelector('#logOutButton').classList.add('loggedOut');
+      this.createFormControls();
+      this.createForm();
     }
   }
-  createFormControls(){
-    this.email = new FormControl("",
+  createFormControls() {
+    this.email = new FormControl('',
             [ Validators.required
-            ])
-    this.password = new FormControl("",
+            ]);
+    this.password = new FormControl('',
                 [ Validators.required,
                   Validators.minLength(6)
-                ])
+                ]);
   }
-  createForm(){
+  createForm() {
     this.loginForm = new FormGroup({
       email: this.email,
       password: this.password
-    })
+    });
   }
-  login(){
+  login() {
 
-    if( this.loginForm.valid )
-    {
-      this.showLoader(true)
+    if ( this.loginForm.valid ) {
+      this.showLoader(true);
       const email = this.email.value.toLowerCase().trim(),
-            password = this.password.value.trim()
+            password = this.password.value.trim();
       /* Tryes to get the user's data from the backend */
       this.Auth.getEncryptedData(email, password).subscribe(data => {
 
-          this.showLoader(false)
-          if(data.success){
-              this.loginError = ""
-              //let ziviDBObj: ZiviDBObj = data.data
-              const userData:string = data.data["encryptedZiviData"]
-              console.log("asd", data, typeof data.data)
-              this.Auth.saveData(userData, password)
-              console.log("data", data)
-              //Display the logout bnutton
-              document.querySelector("#logOutButton").classList.remove("loggedOut")
+          this.showLoader(false);
+          if (data.success) {
+              this.loginError = '';
+              // let ziviDBObj: ZiviDBObj = data.data
+              const userData: string = data.data['encryptedZiviData'];
+              console.log('asd', data, typeof data.data);
+              this.Auth.saveData(userData, password);
+              console.log('data', data);
+              // Display the logout bnutton
+              document.querySelector('#logOutButton').classList.remove('loggedOut');
 
-              //if the backend says everything is ok -> redirect to user's page
-              this.showInputsChecked(true)
-              setTimeout(() => this.router.navigate(["rapportblatt"]), 500)
+              // if the backend says everything is ok -> redirect to user's page
+              this.showInputsChecked(true);
+              setTimeout(() => this.router.navigate(['rapportblatt']), 500);
 
-          }else{
-            this.showInputsChecked(false)
-            this.loginError = data.message
-            console.log(data)
+          } else {
+            this.showInputsChecked(false);
+            this.loginError = data.message;
+            console.log(data);
           }
-      })
+      });
     }
   }
-  showLoader( show:boolean ) {
-    let loadingAnim: HTMLElement = document.querySelector(".loadingAnim")
-    show ?  loadingAnim.style.display = "block" :
-            loadingAnim.style.display = "none"
+  showLoader( show: boolean ) {
+    const loadingAnim: HTMLElement = document.querySelector('.loadingAnim');
+    show ?  loadingAnim.style.display = 'block' :
+            loadingAnim.style.display = 'none';
   }
-  showInputsChecked( show:boolean ) {
-    let loadingAnim: HTMLElement = document.querySelector(".inputsChecked")
-    show ?  loadingAnim.style.display = "block" :
-          loadingAnim.style.display = "none"
+  showInputsChecked( show: boolean ) {
+    const loadingAnim: HTMLElement = document.querySelector('.inputsChecked');
+    show ?  loadingAnim.style.display = 'block' :
+          loadingAnim.style.display = 'none';
   }
 }
--- src/app/rapportblatt/rapportblatt.component.ts
+++ src/app/rapportblatt/rapportblatt.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UserService,
         TableService,
         ExcelService,
-        ImageHandlerService } from '../_services'
+        ImageHandlerService } from '../_services';
 
-interface sendRbResponse{
-  data: any,
-  message: string,
-  success: boolean
+// tslint:disable-next-line:class-name
+interface SendRbResponse {
+  data: any;
+  message: string;
+  success: boolean;
 }
 
 @Component({
@@ -17,8 +18,14 @@ interface sendRbResponse{
 })
 export class RapportblattComponent implements OnInit {
 
-  rows: any
-  sendError: string
+  rows: any;
+  sendError: string;
+  ziviData: any = this.user.getZiviData();
+  todayDate = new Date();
+  monthString = this.table.getMonthString(this.todayDate);
+  today = this.table.getDateString(this.todayDate);
+  ziviName = [this.ziviData.name.firstName, this.ziviData.name.lastName].join(' ');
+  summary: any;
 
   constructor(private user: UserService,
               private table: TableService,
@@ -26,73 +33,67 @@ export class RapportblattComponent implements OnInit {
               private imageHandler: ImageHandlerService) { }
 
   ngOnInit() {
-    this.getTable()
+    this.getTable();
     this.ziviData = this.user.getZiviData();
-    console.log(this.user.getZiviData())
+    console.log(this.user.getZiviData());
   }
-  ziviData = this.user.getZiviData();
-  todayDate = new Date()
-  monthString = this.table.getMonthString(this.todayDate)
-  today = this.table.getDateString(this.todayDate)
-  ziviName = this.ziviData.name.firstName+" "+this.ziviData.name.lastName
-  summary: any
 
   monthChanged(event: Event): void {
 
-    const target = <HTMLInputElement>event.target
-    console.log(target.value)
-    this.monthString = target.value
-    this.getTable()
+    const target = <HTMLInputElement>event.target;
+    console.log(target.value);
+    this.monthString = target.value;
+    this.getTable();
   }
-  getTable(){
-    const locallyStoredRB = localStorage.getItem("savedRapportblatt")
+  getTable() {
+    const locallyStoredRB = localStorage.getItem('savedRapportblatt');
 
-    //default rows config
-    this.rows = this.table.getTableData(this.ziviData, this.monthString)
-    //If there is no RB saved Locally
-    if( locallyStoredRB === null ) {
+    // default rows config
+    this.rows = this.table.getTableData(this.ziviData, this.monthString);
+    // If there is no RB saved Locally
+    if ( locallyStoredRB === null ) {
     // TODO: Multi rapportblatt save
       this.user.getSavedRapportblatt(this.monthString).subscribe( savedRapportblatt => {
-      console.log(savedRapportblatt)
-        if( savedRapportblatt.success ){
-          localStorage.setItem("savedRapportblatt", JSON.stringify(savedRapportblatt.data))
-          if( savedRapportblatt.data.month  === this.monthString ){
-            this.rows = savedRapportblatt.data.rbData
-            console.log(this.rows)
+      console.log(savedRapportblatt);
+        if ( savedRapportblatt.success ) {
+          localStorage.setItem('savedRapportblatt', JSON.stringify(savedRapportblatt.data));
+          if ( savedRapportblatt.data.month  === this.monthString ) {
+            this.rows = savedRapportblatt.data.rbData;
+            console.log(this.rows);
           }
         }
-      })
-    }else{
-      const savedRb = JSON.parse(locallyStoredRB)
-      console.log("savedRb", savedRb)
-      if( savedRb.month  === this.monthString ){
-        this.rows = savedRb.rbData
-        console.log(this.rows)
+      });
+    } else {
+      const savedRb = JSON.parse(locallyStoredRB);
+      console.log('savedRb', savedRb);
+      if ( savedRb.month  === this.monthString ) {
+        this.rows = savedRb.rbData;
+        console.log(this.rows);
       }
-      console.log("RB loaded locally!")
+      console.log('RB loaded locally!');
     }
 
   }
-  send(){
-    
+  send() {
+
     const rapportblattData =  {
                                 ziviName: this.ziviName,
                                 table: this.rows,
                                 summary: this.getSummary(),
                                 month: this.monthString,
                                 shoeMoney: 0 // TODO: Clculate shoemoney
-                              }
-    console.log(rapportblattData)
-    //Validate ToDo
-    if(true){
-      this.showLoader(true)
-      this.showInputsChecked(false)
-        const sheetTitle = "Rapportblatt_" +
-                            rapportblattData.ziviName.replace(" ","_");
+                              };
+    console.log(rapportblattData);
+    // Validate ToDo
+    if ( true ) {
+      this.showLoader(true);
+      this.showInputsChecked(false);
+        const sheetTitle = 'Rapportblatt_' +
+                            rapportblattData.ziviName.replace(' ', '_');
         const excel = this.excel.excelForUpload(
                         this.excel.getExcelFile(rapportblattData),
                         rapportblattData.ziviName,
-                        rapportblattData.month)
+                        rapportblattData.month);
 
         this.excel.sendRapportblatt({   excel:      excel,
                                         excelName:  sheetTitle,
@@ -100,122 +101,121 @@ export class RapportblattComponent implements OnInit {
                                         ziviName:   rapportblattData.ziviName,
                                         abo:        this.ziviData.abo,
                                         month:      rapportblattData.month})
-            .subscribe((data: sendRbResponse) => {
-              
-              this.showLoader(false)
-              if(data.success){
-                this.showInputsChecked(true)
-                this.sendError = ""
+            .subscribe((data: SendRbResponse) => {
+
+              this.showLoader(false);
+              if ( data.success ) {
+                this.showInputsChecked(true);
+                this.sendError = '';
               }
 
-              this.showInputsChecked(false)
-              this.sendError = data.message
-              console.log(JSON.stringify(data))
-            })
+              this.showInputsChecked( false );
+              this.sendError = data.message;
+              console.log(JSON.stringify( data ));
+            });
     }
   }
 
-  save(){
+  save() {
     /** Saves the rows Object on the server and in localStorage**/
     this.user.saveRapportblatt(this.rows, this.monthString).subscribe( data => {
-      console.log(data)
-    })
+      console.log(data);
+    });
   }
 
   onFileSelected(event, date) {
       const target = event.target;
-      const filesOnTarget = target.files
-      console.log(filesOnTarget)
-      for(const file of filesOnTarget){
-          this.imageHandler.addImage(file, date, target)
+      const filesOnTarget = target.files;
+      console.log(filesOnTarget);
+      for ( const file of filesOnTarget) {
+          this.imageHandler.addImage(file, date, target);
       }
   }
 
-  daySummary(sort=false) {
-    const dayTypes = this.getSummary().dayTypes
+  daySummary(sort= false) {
+    const dayTypes = this.getSummary().dayTypes;
     const dayTypesArray  = Object.keys(dayTypes)
             .map((key, index) => {
-              return [key, dayTypes[key]]
-            })
-    function sortedArray(dayTypesArray) {
+              return [key, dayTypes[key]];
+            });
+    function sortedArray( dayTypesArray ) {
       return dayTypesArray.sort((a, b) => {
-                      if(a[1] < b[1]) {
+                      if (a[1] < b[1]) {
                           return 1;
                       }
                       if (a[1] < b[1]) {
                           return -1;
                       }
                       return 0;
-                    })
+                    });
     }
-    return sort ? sortedArray(dayTypesArray) : dayTypesArray
+    return sort ? sortedArray(dayTypesArray) : dayTypesArray;
   }
 
-  openSlideshow(){
-      console.log("openSlideshow!")
+  openSlideshow() {
+      console.log('openSlideshow!');
   }
 
-  getSummary(){
+  getSummary() {
     // Normalentschädigung ist 25Fr./Tag
     const normalPay = 25;
     const urlaubPay = 0;
 
     const spesenPreis = this.rows.reduce((previous, o) => {
-      if(o["price"] != '')
-        { return previous + o["price"] }
-      return previous
-    }, 0)
+      if (o['price'] !== '') { return previous + o['price']; }
+      return previous;
+    }, 0);
 
     const pay = this.rows.reduce((previous, o) => {
-      if(o["dayType"] != 'Urlaub')
-        { return previous + normalPay }
-      return previous
-    }, 0)
+      if (o['dayType'] !== 'Urlaub') { return previous + normalPay; }
+      return previous;
+    }, 0);
 
-    const shoeMoney = 0 // TODO: schuhgeld berechnung
+    const shoeMoney = 0; // TODO: schuhgeld berechnung
 
     this.summary =  {
       dayTypes:
       {
-        krankTage: this.rows.reduce((previous, o) => (o["dayType"] == "Krank")
-                                                        ? previous+1
+        krankTage: this.rows.reduce((previous, o) => (o['dayType'] === 'Krank')
+                                                        ? previous + 1
                                                         : previous, 0),
-        freiTage: this.rows.reduce((previous, o) => (o["dayType"] == "Frei")
-                                                        ? previous+1
+        freiTage: this.rows.reduce((previous, o) => (o['dayType'] === 'Frei')
+                                                        ? previous + 1
                                                         : previous, 0),
-        ferienTage: this.rows.reduce((previous, o) => (o["dayType"] == "Ferien")
-                                                        ? previous+1
+        ferienTage: this.rows.reduce((previous, o) => (o['dayType'] === 'Ferien')
+                                                        ? previous + 1
                                                         : previous, 0),
-        urlaubstage: this.rows.reduce((previous, o) => (o["dayType"] == "Urlaub")
-                                                        ? previous+1
+        urlaubstage: this.rows.reduce((previous, o) => (o['dayType'] === 'Urlaub')
+                                                        ? previous + 1
                                                         : previous, 0),
-        arbeitsTage: this.rows.reduce((previous, o) => (o["dayType"] == "Arbeitstag")
-                                                          ? previous+1
+        arbeitsTage: this.rows.reduce((previous, o) => (o['dayType'] === 'Arbeitstag')
+                                                          ? previous + 1
                                                           : previous, 0)
       },
 
-      //total daily compensation
+      // total daily compensation
       compensation: pay,
-      //Add all the 'Fahrspesen' up
+      // Add all the 'Fahrspesen' up
       spesenPreis: spesenPreis,
 
       shoes: shoeMoney,
 
       total: spesenPreis + pay + shoeMoney
-    }
-    return this.summary
+    };
+    return this.summary;
   }
 
-  showLoader( show:boolean ) {
-    let loadingAnim: HTMLElement = document.querySelector(".loadingAnim")
-    show ?  loadingAnim.style.display = "block" :
-            loadingAnim.style.display = "none"
+  showLoader( show: boolean ) {
+    const loadingAnim: HTMLElement = document.querySelector('.loadingAnim');
+    show ?  loadingAnim.style.display = 'block' :
+            loadingAnim.style.display = 'none';
   }
-  showInputsChecked( show:boolean ) {
-    let loadingAnim: HTMLElement = document.querySelector(".inputsChecked")
-    show ?  loadingAnim.style.display = "block" :
-          loadingAnim.style.display = "none"
+  showInputsChecked( show: boolean ) {
+    const loadingAnim: HTMLElement = document.querySelector('.inputsChecked');
+    show ?  loadingAnim.style.display = 'block' :
+          loadingAnim.style.display = 'none';
   }
 
-  getPercentage(a, b){ return b > 0 ?  Math.floor(a/b*100).toString()+"%" : "0%" }
+  getPercentage(a, b) { return b > 0 ?  Math.floor(a / b * 100).toString() + '%' : '0%'; }
+
 }
